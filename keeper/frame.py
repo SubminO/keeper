7fada42e2ea26da17868b67606ee76ddb081239c
Iurii Shikin <shikin@cpan.org>
2019-08-11 15:22:39 +0400

    3 commit

--- keeper/frame.py
+++ keeper/frame.py
@@ -4,11 +4,20 @@ import json
 class Frame:
     _fields = {'vin', 'speed', 'longitude', 'lattitude'}
 
-    def __init__(self, data):
-        self._data = json.dumps(data.decode("utf-8").replace("][", ","))
+    def __init__(self, string):
+        data = json.loads(string.decode("utf-8").replace("][", ","))
+        self._data = {vd['vin']: vd for vd in data if self._is_valid(vd)}
 
-    def init(self):
-        return {vd['vin']: vd for vd in self._data if self._is_valid(vd)}
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __iter__(self):
+        return self._data
+        # for key in self._data.keys():
+        #     yield key
+
+    def __len__(self):
+        return len(self._data.keys())
 
     def _is_valid(self, vd):
         return self._fields.issubset(vd.keys())
